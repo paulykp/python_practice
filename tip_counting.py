e2c725778c56a3eeb685b245557dd4a0192ef112
Tobin <tobinrisser@gmail.com>
2019-01-25 15:03:31 -0800

    added formatting for Calculator output.

--- tip_counting.py
+++ tip_counting.py
@@ -2,19 +2,18 @@
 #weekday & Saturday tip counting
 def tc(tip1, tip2, tip3, tip4, tip5):
   
-  open_1 = tip1 + tip2 + tip3
-  open_3 = open_1 + tip4
-  10_close = tip2 + tip3 + tip4 + tip5
-  12_close = 10_close - tip2
-
-  print(open_1, open_3, 10_close, 12_close)
+    open_one = tip1 + tip2 + tip3
+    open_three = open_one + tip4
+    ten_close = tip2 + tip3 + tip4 + tip5
+    noon_close = ten_close - tip2
+    print(" Shift 1 : %.2f \n Shift 2 : %.2f \n Shift 3 : %.2f \n Shift 4 : %.2f \n \n" % (open_one, open_three, ten_close, noon_close))
 
 #Sunday tip counting
 def sunday(tip1, tip2, tip3):
 	
 	all_day = tip1 + tip2 + tip3
-	open_1 = tip1 + tip2
-	10_close = all_day - tip1
+	open_one = tip1 + tip2
+	ten_close = all_day - tip1
 	print(open_1, all_day, 10_close)
 
 #hi tobin
