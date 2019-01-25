01f26c21cc4937bfa7bb619da3b98bf7f7fbbb1a
Paul Pak <paulpak@MacBook-Pro.local>
2019-01-25 11:27:54 -0800

    blah blah

--- tip_counting.py
+++ tip_counting.py
@@ -1,6 +1,7 @@
 
 #weekday & Saturday tip counting
 def tc(tip1, tip2, tip3, tip4, tip5):
+  
   open_1 = tip1 + tip2 + tip3
   open_3 = open_1 + tip4
   10_close = tip2 + tip3 + tip4 + tip5
@@ -10,9 +11,11 @@ def tc(tip1, tip2, tip3, tip4, tip5):
 
 #Sunday tip counting
 def sunday(tip1, tip2, tip3):
+	
 	all_day = tip1 + tip2 + tip3
 	open_1 = tip1 + tip2
 	10_close = all_day - tip1
 	print(open_1, all_day, 10_close)
 
+#hi tobin
 
