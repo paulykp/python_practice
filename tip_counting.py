aca5a36e76c8794aa72ff61f5703a66ce0bef4e7
tobinr <46064060+tobinr@users.noreply.github.com>
2019-01-24 21:40:49 -0800

    Add files via upload
    tip counting calculator for Anthem

---
+++ tip_counting.py
@@ -0,0 +1,18 @@
+
+#weekday & Saturday tip counting
+def tc(tip1, tip2, tip3, tip4, tip5):
+  open_1 = tip1 + tip2 + tip3
+  open_3 = open_1 + tip4
+  10_close = tip2 + tip3 + tip4 + tip5
+  12_close = 10_close - tip2
+
+  print(open_1, open_3, 10_close, 12_close)
+
+#Sunday tip counting
+def sunday(tip1, tip2, tip3):
+	all_day = tip1 + tip2 + tip3
+	open_1 = tip1 + tip2
+	10_close = all_day - tip1
+	print(open_1, all_day, 10_close)
+
+
